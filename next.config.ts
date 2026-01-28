ea34e155b1887fb22f052d64fe71deccc007be22
gyamoto1054 <gya.motoyama@gmail.com>
2026-01-28 15:27:49 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
+  output: 'export',  // これを追加：静的サイトとして書き出す設定
+  images: {
+    unoptimized: true, // これを追加：画像のエラーを防ぐ
+  },
 };
 
 export default nextConfig;
